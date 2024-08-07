164f0f90cc354fed349e810439521712b36910eb
Saurabh8807 <sjagtap18092002@gmail.com>
2024-08-07 19:33:21 +0530

    changes

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import routes from "./src/routes/index.routes.js"
+import cors from 'cors';
 
 const app = express();
+
+// Enable CORS
+app.use(cors({
+    origin: 'http://localhost:5173', // Your client origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+    credentials: true // Allow cookies to be sent
+  }));
 
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
